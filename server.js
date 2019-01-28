9df8031ecc9c65283ea14a8170e6b69734bc0d28
Julius Burbulis <burbulis.julius@gmail.com>
2019-01-28 11:08:12 +0200

    add body-parser config

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const databaseUser = 'bta-studentas';
+const databasePassword = 'studentas123';
+const databaseName = 'my-bta-database';
+const databasePort = 57862;
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 // database config
 mongoose.connect(
-  'mongodb://bta-studentas:studentas123@ds057862.mlab.com:57862/my-bta-database',
+  `mongodb://${databaseUser}:${databasePassword}@ds057862.mlab.com:${databasePort}/${databaseName}`,
   { useNewUrlParser: true }
 );
 
