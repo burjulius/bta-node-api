9a4d13fbf6a4b6bf65dddbf03de356a4fe5b76bb
Julius Burbulis <burbulis.julius@gmail.com>
2019-01-29 10:22:57 +0200

    add book model

---
+++ models/book.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: String,
+  author: String,
+  year: Date,
+  category: String,
+  bestSeller: Boolean,
+});
+
+module.exports = mongoose.model('Boook', BookSchema);
